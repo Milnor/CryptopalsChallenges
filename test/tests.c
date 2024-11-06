b554422183377d0b777f0d272e69ae62a7f00a49
Milnor <Milnor@users.noreply.github.com>
2024-11-06 12:14:05 -0500

    refactor tests

--- test/tests.c
+++ test/tests.c
@@ -5,107 +5,81 @@
 
 #include "../libs/basics.h"
 
+// Test function that takes 1 string as input
+void test_func_1param(const char * name, const char * expected, const char * input1, char * (*func)(char *))
+{
+    char * output = func(input1);
+    
+    if (strncmp(output, expected, strlen(expected)))
+    {
+        fprintf(stderr, " [-] %s test failed:\n"
+                        "  Expected: %s\n"
+                        "  Actual: %s\n",
+                        name, expected, output);
+    }
+    else
+    {
+        printf(" [+] %s test passed\n", name);
+    }
+
+    free(output);
+}
+
+// Test function that takes 2 strings as inputs
+void test_func_2params(const char * name, const char * expected, const char * input1, const char * input2, char * (*func)(char *, char *))
+{
+    char * output = func(input1, input2);
+    
+    if (strncmp(output, expected, strlen(expected)))
+    {
+        fprintf(stderr, " [-] %s test failed:\n"
+                        "  Expected: %s\n"
+                        "  Actual: %s\n",
+                        name, expected, output);
+    }
+    else
+    {
+        printf(" [+] %s test passed\n", name);
+    }
+
+    free(output);
+}
+
 int main()
 {
-    printf("Tests for Set 1: Basics\n");
-    printf(" [!] Testing 1.1 Hex to Base64...\n");
+    printf("New test function...\n");
+
+    /* 1.1 Convert hex to base64 */
 
     // No padding example from https://cryptopals.com/sets/1/challenges/1
+    test_func_1param("Hex string to base64 - no pad",
+                     "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29u"  
+                     "b3VzIG11c2hyb29t",
+                     "49276d206b696c6c696e6720796f757220627261696e206c6"  
+                     "96b65206120706f69736f6e6f7573206d757368726f6f6d",
+                     hex_to_base64);
+
     
-    int successes = 0;
-
-    //uint8_t pad0_input[] = "\x49\x27\x6d\x20\x6b\x69\x6c\x6c\x69\x6e\x67\x20"
-    //                       "\x79\x6f\x75\x72\x20\x62\x72\x61\x69\x6e\x20\x6c"
-    //                       "\x69\x6b\x65\x20\x61\x20\x70\x6f\x69\x73\x6f\x6e"
-    //                       "\x6f\x75\x73\x20\x6d\x75\x73\x68\x72\x6f\x6f\x6d";
-    uint8_t pad0_input[] = "49276d206b696c6c696e6720796f757220627261696e206c6"
-                            "96b65206120706f69736f6e6f7573206d757368726f6f6d";
-    uint8_t pad0_output[] = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29u"
-                            "b3VzIG11c2hyb29t";
-
-    char * output = hex_to_base64(pad0_input);
-    
-    if (strncmp(output, pad0_output, strlen(pad0_output)))
-    {
-        // Failure
-        fprintf(stderr, " [-] No padding test failed:\n"
-                        "  Expected: %s\n"
-                        "  Actual: %s\n",
-                        pad0_output, output);
-    }
-    else
-    {
-        successes++;
-        printf(" [+] No padding test passed\n");
-    }
-
-    free(output);
-
     // Examples with padding from https://en.wikipedia.org/wiki/Base64
-    uint8_t pad1_input[] = "6c6967687420776f726b2e"; // "light work." 
-    uint8_t pad1_output[] = "bGlnaHQgd29yay4=";
-    output = hex_to_base64(pad1_input);
+    test_func_1param("Hex string to base64 - 1 pad",
+                     "bGlnaHQgd29yay4=",
+                     "6c6967687420776f726b2e",      // "light work."
+                     hex_to_base64);
     
-    if (strncmp(output, pad1_output, strlen(pad1_output)))
-    {
-        fprintf(stderr, " [-] Pad 1 test failed:\n"
-                        "  Expected: %s\n"
-                        "  Actual: %s\n",
-                        pad1_output, output);
-    }
-    else
-    {
-        successes++;
-        printf(" [+] Pad 1 test passed\n");
-    }
 
-    free(output);
+    test_func_1param("Hex string to base64 - 2 pad",
+                     "bGlnaHQgd29yaw==",
+                     "6c6967687420776f726b",        // "light work" 
+                     hex_to_base64);
 
-    uint8_t pad2_input[] = "6c6967687420776f726b"; // "light work" 
-    uint8_t pad2_output[] = "bGlnaHQgd29yaw==";
-    output = hex_to_base64(pad2_input);
 
-    if (strncmp(output, pad2_output, strlen(pad2_output)))
-    {
-        fprintf(stderr, " [-] Pad 2 test failed:\n"
-                        "  Expected: %s\n"
-                        "  Actual: %s\n",
-                        pad2_output, output);
-    }
-    else
-    {
-        successes++;
-        printf(" [+] Pad 2 test passed\n");
-    }
+    /* 1.2 Fixed XOR */
+    
+    test_func_2params("Fixed XOR",
+                      "746865206b696420646f6e277420706c6179",
+                      "1c0111001f010100061a024b53535009181c",
+                      "686974207468652062756c6c277320657965",
+                      fixed_xor);
 
-    free(output);
-
-    printf(" [!] %d of 3 tests passed for Hex to Base64\n", successes);
-    successes = 0;
-
-    printf(" [!] Testing 1.2 Fixed XOR...\n");
-
-    char buffer[] = "1c0111001f010100061a024b53535009181c";
-    char xor_key[] = "686974207468652062756c6c277320657965";
-    char answer[] =  "746865206b696420646f6e277420706c6179";
-
-    output = fixed_xor(buffer, xor_key);
-
-    if (strncmp(output, answer, strlen(answer)))
-    {
-        fprintf(stderr, " [-] Fixed XOR test failed:\n"
-                        "  Expected: %s\n"
-                        "  Actual: %s\n",
-                        answer, output);
-    }
-    else
-    {
-        successes++;
-        printf(" [+] Fixed XOR test passed\n");
-    }
-
-    free(output);
-
-    printf(" [!] %d of 1 tests passed for Fixed XOR\n", successes);
 }
 
